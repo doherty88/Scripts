659250ebc2d832cac7d23e9b4074c3d09bb9230c
doherty88 <liudeyang@gmail.com>
2020-11-12 09:30:40 +0800

    fix typo in $request

--- quanx/changtai.js
+++ quanx/changtai.js
@@ -25,8 +25,7 @@ if ($cmp.isRequest) {
 
 function GetToken() {
     if ($request && $request.method == "POST") {
-        $cmp.notify("Step 1", "", "")
-        var bodyJson = JSON.parse(request.body)
+        var bodyJson = JSON.parse($request.body)
         var TokenValue = bodyJson.data.access_token
         $cmp.notify("Step 2", "", "")
         if ($cmp.read(accessTokeName) != (undefined || null)) {
