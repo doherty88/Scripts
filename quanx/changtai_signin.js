bc6c5b2e8f611bc488e1b3ee6e54df8ba7027764
doherty88 <liudeyang@gmail.com>
2021-01-04 02:23:49 +0000

    Update changtai_signin.js

--- quanx/changtai_signin.js
+++ quanx/changtai_signin.js
@@ -1,1 +1,29 @@
+const $cmp = compatibility()
+const CheckinURL = 'https://api.techmall.chamshare.cn/daysign/sign'
+const accessTokeName = 'changtaikey'
 
+const url = CheckinURL;
+const method = "PUT";
+const headers = {
+            'Content-Type': 'application/json',
+            'Host' : "api.techmall.chamshare.cn"
+        };
+const data = {"access_token": $cmp.read(accessTokeName)};
+
+const myRequest = {
+    url: url,
+    method: method, // Optional, default GET.
+    headers: headers, // Optional.
+    body: JSON.stringify(data) // Optional.
+};
+
+$task.fetch(myRequest).then(response => {
+    // response.statusCode, response.headers, response.body
+    console.log(response.body);
+    $notify("Title", "Subtitle", response.body); // Success!
+    $done();
+}, reason => {
+    // reason.error
+    $notify("Title", "Subtitle", reason.error); // Error!
+    $done();
+});
